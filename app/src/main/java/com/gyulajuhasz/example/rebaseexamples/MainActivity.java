bd140bcafa601966aabf273d69c300305132bb4b
Gyula Juháasz <gyu999@gmail.com>
2017-06-09 00:38:09 +0200

    Initial commit.

---
+++ app/src/main/java/com/gyulajuhasz/example/rebaseexamples/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gyulajuhasz.example.rebaseexamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
