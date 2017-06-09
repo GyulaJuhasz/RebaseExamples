4bdb275cb8c289f52d3513a3951fae0ac02391f4
Gyula Juháasz <gyu999@gmail.com>
2017-06-09 03:42:16 +0200

    Liness added.

--- app/src/main/java/com/gyulajuhasz/example/rebaseexamples/MainActivity.java
+++ app/src/main/java/com/gyulajuhasz/example/rebaseexamples/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Feature 2 line 1
+        // Feature 2 Line 2
+        // Feature 2 Line 3
+        // Feature 2 Line 4
         setContentView(R.layout.activity_main);
         // New commit for master
     }
